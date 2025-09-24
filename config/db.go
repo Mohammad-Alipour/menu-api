ade40b54b66c99dc3d7dd4ee177471b8c3a71259
Mohammad-Alipour <imoalipour@gmail.com>
2025-09-24 09:12:42 +0330

    feat: Add new field to user model

--- config/db.go
+++ config/db.go
@@ -2,24 +2,25 @@ package config
 
 import (
 	"fmt"
-	"log"
-
+	"github.com/Mohammad-Alipour/menu-api/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 )
 
-var DB *gorm.DB
+var _ *gorm.DB
 
 func ConnectDatabase() {
-	//DatabaseConnectionInfo
 	dsn := "host=localhost user=postgres password=1234 dbname=menu_api port=5432 sslmode=disable TimeZone=Asia/Tehran"
 
-	//ConnectDatabase
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("❌ اتصال به دیتابیس ناموفق بود: ", err)
 	}
 
-	DB = db
-	fmt.Println("✅ اتصال به دیتابیس برقرار شد")
+	// Migration
+	db.AutoMigrate(&models.User{})
+
+	_ = db
+	fmt.Println("✅ اتصال به دیتابیس برقرار شد و Migration انجام شد")
 }
